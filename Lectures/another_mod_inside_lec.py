60a7d6c066051a441c56a85fc012086833b010ea
Maoheng Xu <kylinxmh@gmail.com>
2023-10-24 21:14:32 +1100

    Initial commit

---
+++ Lectures/another_mod_inside_lec.py
@@ -0,0 +1,14 @@
+""" another_mod_inside_lec.py
+
+Another module inside the `lectures` package
+"""
+
+#
+# from Lectures.mod_inside_lec import APIKEY
+# # print(APIKEY)
+
+# s ="\u00A9"
+# print(s)
+
+from mod_inside_lec import APIKEY
+print(APIKEY)
---
+++ Lectures/mod_inside_lec.py
@@ -0,0 +1,6 @@
+""" mod_inside_lec.py
+
+A module inside the `lectures` package
+"""
+APIKEY = "123"
+print(APIKEY)
